26b6f50fd4413385a31ec5c6af5c8ef70b23afb2
Timmy O'Mahony <hey@timmyomahony.com>
2020-06-24 19:28:31 +0100

    Removed pyenergenie and added submodule

--- lib/pyenergenie/src/energenie/crypto_test.py
+++
@@ -1,8 +0,0 @@
-# crypto_test.py  21/05/2016  D.J.Whale
-#
-# Placeholder for test harness for crypto.py
-
-#TODO:
-
-print("no tests defined")
-# END
--- lib/switch.py
+++ lib/switch.py
@@ -4,23 +4,19 @@ import time
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/pyenergenie/src')
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/energenie-switches/v1')
 
-import energenie
-energenie.init()
-
-me_global = sys.modules[__name__]
-energenie.registry.load_into(me_global)
+from switch import on, off
 
 try:
     if len(sys.argv) == 3:
         if sys.argv[1] == 'on':
             print("switch " + sys.argv[2] + " on")
-            exec(sys.argv[2] + '.turn_on()')
+            exec('on(socket={}'.format(sys.argv[2]))
         elif sys.argv[1] == 'off':
             print("switch " + sys.argv[2] + " off")
-            exec(sys.argv[2] + '.turn_off()')
+            exec('off(socket={}'.format(sys.argv[2]))
     else:
         print("invalid number of arguments")
-finally:
-    energenie.finished()
+except:
+    pass
