67e3ee9e29b2b9a75edea0db510ed88bb32c8f6e
Timmy O'Mahony <hey@timmyomahony.com>
2020-06-24 19:59:14 +0100

    Updated switch

--- lib/switch.py
+++ lib/switch.py
@@ -8,17 +8,14 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/energenie-swi
 
 from switch import on, off
 
-try:
-    if len(sys.argv) == 3:
-        if sys.argv[1] == 'on':
-            print("switch " + sys.argv[2] + " on")
-            on(socket=int(sys.argv[2]))
-            print("ON!")
-        elif sys.argv[1] == 'off':
-            print("switch " + sys.argv[2] + " off")
-            off(socket=int(sys.argv[2]))
-            print("OFF!")
-    else:
-        print("invalid number of arguments")
-except:
-    raise
+if len(sys.argv) == 3:
+    if sys.argv[1] == 'on':
+        print("switch " + sys.argv[2] + " on")
+        on(socket=int(sys.argv[2]))
+        print("ON!")
+    elif sys.argv[1] == 'off':
+        print("switch " + sys.argv[2] + " off")
+        off(socket=int(sys.argv[2]))
+        print("OFF!")
+else:
+    print("invalid number of arguments")
